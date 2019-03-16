a031a7fe140d05777c57a68cabebc458b4348196
ohatakky <ohatakky@gmail.com>
2019-03-16 19:58:05 +0900

    委譲と埋め込みによるダックタイピング

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+type SmartPhone interface {
+	Read()
+	Play()
+	Search()
+}
+
+type IPhone6 struct {
+	Size int64
+}
+type Animal struct{}
+
+// IPhone6はRead(), Play(), Search()を実装したのでSmartPhoneを満たす
+func (i IPhone6) Read() {
+	fmt.Println("read a book")
+}
+
+func (i IPhone6) Play() {
+	fmt.Println("play a music")
+}
+
+func (i IPhone6) Search() {
+	fmt.Println("search for information")
+}
+
+func duckSmartPhone(s SmartPhone) {
+	switch x := s.(type) {
+	case IPhone6:
+		fmt.Println(x.Size)
+	default:
+		fmt.Println("not IPhone6")
+	}
+}
+
+type deligate_IPhone7 struct {
+	i6     IPhone6
+	FeliCa bool
+}
+
+type embedded_IPhone7 struct {
+	IPhone6
+	FeliCa bool
+}
+
+func main() {
+	iphone6 := IPhone6{Size: 10}
+	duckSmartPhone(iphone6)
+
+	// neko := Animal{}
+	// duckSmartPhone(neko) // SmartPhoneを満たしていないのでエラー
+
+	// 委譲によるダックタイピング
+	de_iphone7 := deligate_IPhone7{i6: iphone6, FeliCa: true}
+	duckSmartPhone(de_iphone7.i6)
+
+	// 埋め込みによるダックタイピング
+	em_iphone7 := embedded_IPhone7{}
+	duckSmartPhone(em_iphone7)
+	duckSmartPhone(em_iphone7.IPhone6) // 予期せぬ動作
+}
